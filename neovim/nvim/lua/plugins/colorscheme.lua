afe23405142c22c71dd9ca026303c2da3418f9a2
Muzych <haxk1024@gmail.com>
2025-07-10 14:39:58 +0800

    feat: change the neovim configs

---
+++ neovim/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require("tokyonight").setup({
+            style = "moon", -- "storm", "moon", "night", "day"
+        })
+
+        vim.cmd.colorscheme("tokyonight")
+    end,
+}
